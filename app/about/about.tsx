d82830ac507f9080dd68aa90804f671ff2c7d956
guendouz bachir <guendouzbachir@hotmail.com>
2024-11-02 06:45:24 +0100

    add mypage

---
+++ app/about/about.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link"
+
+function About() {
+    
+    return <>
+            <Link href="/about">About</Link>
+            <div>About</div>
+        </>
+  }
+   
+  export default About
